49621ac9ff047556b95a56d0810547f40c881bd1
xyang5 <xyang5@monsanto.com>
2019-10-31 16:22:27 -0500

    finished auth flow

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/user");
 require("./services/passport");
@@ -14,6 +16,15 @@ mongoose
   });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // time cookie to last
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
