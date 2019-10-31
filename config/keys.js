fe05934c4c0b4172dc054d6d30179b43d5d08c71
xyang5 <xyang5@monsanto.com>
2019-10-31 10:35:46 -0500

    .gitignore is now working

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '971270137424-3i4imbf0m0mb9ombdb2b1jnop83ufah8.apps.googleusercontent.com',
-    googleClientSecret:'aWYMjmorcEEkKjJizQrw4QCV',
-    mongoURI: 'mongodb+srv://emailyUser:Password@emaily-vepoe.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'redacted'
-};
